975f625e2d48bf04734501605fdde9343c05b1ff
Danil Menkin <danil.menkin@arkis.xyz>
2023-12-29 11:52:30 +0400

    1st stage done

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
-// Uncomment this block to pass the first stage
-// use std::net::UdpSocket;
+use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 2053);
+    let udp_socket = UdpSocket::bind(addr).expect("Failed to bind to address");
+    println!("Successfully bound to address: {:?}", addr);
+    let mut buf = [0; 512];
 
-    // Uncomment this block to pass the first stage
-    // let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    // let mut buf = [0; 512];
-    //
-    // loop {
-    //     match udp_socket.recv_from(&mut buf) {
-    //         Ok((size, source)) => {
-    //             println!("Received {} bytes from {}", size, source);
-    //             let response = [];
-    //             udp_socket
-    //                 .send_to(&response, source)
-    //                 .expect("Failed to send response");
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error receiving data: {}", e);
-    //             break;
-    //         }
-    //     }
-    // }
+    loop {
+        match udp_socket.recv_from(&mut buf) {
+            Ok((size, source)) => {
+                println!("Received {} bytes from {}", size, source);
+                let response = [];
+                udp_socket
+                    .send_to(&response, source)
+                    .expect("Failed to send response");
+            }
+            Err(e) => {
+                eprintln!("Error receiving data: {}", e);
+                break;
+            }
+        }
+    }
 }
